ca9bbe53a4dcd1c5d9963cece141ada148a524d4
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-08-30 22:54:18 +0930

    update life of resetToken

--- nodeServer/dbconnet/nodeCache.js
+++ nodeServer/dbconnet/nodeCache.js
@@ -3,7 +3,7 @@ const NodeCache = require("node-cache");
 /**
  * Time to Live for the cache.
  */
-const timeInMin = 5
+const timeInMin = 30
 
 /**
  * Install node cache used for reset link
@@ -12,6 +12,7 @@ const myCache = new NodeCache({
     stdTTL: timeInMin*60, 
     checkperiod: 20*60*60,
     useClones: false,
+    deleteOnExpire: true
 });
 
 module.exports = myCache;
