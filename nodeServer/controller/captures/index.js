a511958ac724c79d0435167b691a5195b8126d41
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-09-05 15:28:48 +0930

    implement image compression

--- nodeServer/controller/captures/index.js
+++ nodeServer/controller/captures/index.js
@@ -6,8 +6,8 @@ const operations = {};
 const multer = require('multer');
 
 const upload = multer({
-    dest:  path.join(__dirname, '../../public/captures'),
-    limits: { fileSize: 5 * 1024 * 1024 }
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 10 * 1024 * 1024 }
 });
 
 fs.readdirSync(__dirname)
