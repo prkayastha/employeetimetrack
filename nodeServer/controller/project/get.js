24298c123cf3314d3ff8f8db65ce3b3af67ed71a
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-09-08 21:32:18 +0930

    project include owner and assignee

--- nodeServer/controller/project/get.js
+++ nodeServer/controller/project/get.js
@@ -2,8 +2,14 @@ const models = require('../../models');
 const UnauthorizedError = require("../../prototypes/responses/authorization/unauthorized");
 const ProjectNotFoundError = require('../../prototypes/responses/project/project-not-found.error');
 
-module.exports = async function(operatorInfo, projectId) {
-    let project = await models.Project.findOne({ where: { id: projectId } });
+module.exports = async function (operatorInfo, projectId) {
+    let project = await models.Project.findOne({
+        include: [
+            { model: models.Users, as: 'projectOwner', attributes: ['id', 'firstname', 'lastname'] },
+            { model: models.Users, as: 'assignees', attributes: ['id', 'firstname', 'lastname'] }
+        ],
+        where: { id: projectId }
+    });
     if (!project) {
         const error = new ProjectNotFoundError();
         throw error;
@@ -11,5 +17,15 @@ module.exports = async function(operatorInfo, projectId) {
     if (project.projectOwnerUserId != operatorInfo.id && project.createdByUserId != operatorInfo.id) {
         throw new UnauthorizedError("Unauthorized to access the project");
     }
-    return project;
+    const projectOwner = {
+        id: project.projectOwner.id,
+        name: `${project.projectOwner.firstname} ${project.projectOwner.lastname}`
+    }
+
+    let assignees = null;
+    if (!!project.assignees) {
+        assignees = project.assignees.map(assignee => ({ id: assignee.id, name: `${assignee.firstname} ${assignee.lastname}` }));
+    }
+
+    return {...project.dataValues, projectOwner, assignees};
 };
