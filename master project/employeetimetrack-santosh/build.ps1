e4ecde341cbe9b600335c27b12b12705c82f1be3
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-09-09 17:13:47 +0930

    delete nested project

--- master project/employeetimetrack-santosh/build.ps1
+++
@@ -1,5 +0,0 @@
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 448346412494.dkr.ecr.us-east-2.amazonaws.com
-
-docker-compose build
-
-docker-compose push
