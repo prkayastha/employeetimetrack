3524bbe920a57537ea7609a7ca2a33f491b7d35f
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-09-12 03:39:33 +0930

    add pattern validator for password

--- Angular/src/app/modules/authentication/pages/register/register.component.ts
+++ Angular/src/app/modules/authentication/pages/register/register.component.ts
@@ -30,7 +30,7 @@ export class RegisterComponent implements OnInit {
             firstname: ['', Validators.required],
             lastname: ['', Validators.required],
             email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(8)]],
+            password: ['', [Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-@$!%*?&])[A-Za-z\\d@$!%*?&-]{8,}")]],
             confirmPassword: ['', Validators.required],
             acceptTerms: [false, Validators.requiredTrue]
         }, {
