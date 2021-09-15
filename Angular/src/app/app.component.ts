f5229c65c6371caf7ec54f028453643d14465036
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-09-15 15:11:43 +0930

    add external js file to start capturing screen

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+declare function startCapture(displayMediaOptions): any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Employee Time Tracking App';
+
+  ngOnInit(): void {
+    /* const displayMediaOptions = {
+      cursor: 'always',
+      displaySurface: 'monitor'
+    };
+
+    startCapture(displayMediaOptions).then(([stream, video]) => {
+      console.log('view value');
+    }) */
+  }
 }
---
+++ Angular/src/assets/screenshot.js
@@ -0,0 +1,64 @@
+const video = document.querySelector('#screenshot video');
+
+async function startCapture(displayMediaOptions) {
+    let captureStream = null;
+    let video = document.createElement('video');
+    video.style.cssText = "display: none; width: 1px; height: 1px;";
+
+    let caputerInterval = null;
+
+    try {
+        captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+
+        video.srcObject = captureStream;
+
+        caputerInterval = setInterval(() => {
+            let base64Img = captureSnapShot(video);
+            localStorage.setItem('captureImage', base64Img);
+        }, 5000);
+
+        captureStream.oninactive = () => {
+            console.log('Streaming stopped');
+            localStorage.removeItem('captureImage')
+            stopCapture(video);
+            if (!!caputerInterval) {
+                debugger;
+                console.log('clearing interval')
+                clearInterval(caputerInterval);
+            }
+        };
+
+    } catch (err) {
+        console.error("Error: " + err);
+        video.remove();
+    }
+
+    return [captureStream, video];
+}
+
+function stopCapture(videoElement) {
+    let tracks = videoElement.srcObject.getTracks();
+
+    tracks.forEach(tracks => tracks.stop());
+    videoElement.srcObject = null;
+    videoElement.remove();
+}
+
+function captureSnapShot(videoElement) {
+    const canvas = document.createElement('canvas');
+    try {
+        canvas.style.cssText = "desplay: none;";
+        canvas.width = videoElement.videoWidth;
+        canvas.height = videoElement.videoHeight;
+
+        canvas.getContext('2d').drawImage(videoElement, 0, 0);
+
+        let dataUrl = canvas.toDataURL('image/png');
+        return dataUrl;
+    } catch(err) {
+        console.log(err)
+        return null;
+    } finally {
+        canvas.remove();
+    }
+}
