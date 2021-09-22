d4f5da8fcc90f77d7974b7fb3a8074ee0ca7b7a5
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-09-22 20:01:23 +0930

    get project involvement by day

--- nodeServer/routes/dashboard.js
+++ nodeServer/routes/dashboard.js
@@ -3,14 +3,20 @@ const express = require('express'),
 const dashbaord = require('../controller/dashboard');
 const errorHandler = require('../controller/errorHandler');
 const jwtDecoder = require('../utils/jwt-decode');
+const momentTz = require('moment-timezone');
 
 router.get('/', async (req, res) => {
     const jwtPayload = jwtDecoder(req);
+    const localTimeOffset = momentTz(momentTz(), momentTz.tz.guess()).format('Z');
     try {
-        const assignedProjects = await dashbaord.getAssignedProject(jwtPayload.id);
-        const workedOnProject = await dashbaord.getWorkedOnProject(jwtPayload.id);
-        const projectInvovlement = await dashbaord.getProjectInvolvementForWeek(jwtPayload.id, '+09:30')
-        res.send({assignedProjects, workedOnProject, projectInvovlement});
+        const queryCollection = [
+            dashbaord.getAssignedProject(jwtPayload.id),
+            dashbaord.getWorkedOnProject(jwtPayload.id),
+            dashbaord.getProjectInvolvementForWeek(jwtPayload.id, localTimeOffset),
+            dashbaord.getProjectHrByDay(jwtPayload.id, localTimeOffset)
+        ];
+        const [assignedProjects, workedOnProject, projectInvovlement, projectHrByDay] = await Promise.all(queryCollection);
+        res.send({assignedProjects, workedOnProject, projectInvovlement, projectHrByDay});
     } catch (error) {
         errorHandler(res, error);
     }
