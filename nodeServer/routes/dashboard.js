2ffaff44be0bc6682bb6c423bc850de5cfe33202
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-09-23 13:01:05 +0930

    add endpoint for weekly report

--- nodeServer/routes/dashboard.js
+++ nodeServer/routes/dashboard.js
@@ -1,19 +1,30 @@
 const express = require('express'),
     router = express.Router();
 const dashbaord = require('../controller/dashboard');
+const report = require('../controller/report');
 const errorHandler = require('../controller/errorHandler');
 const jwtDecoder = require('../utils/jwt-decode');
 const momentTz = require('moment-timezone');
+
+router.get('/report', async (req, res) => {
+    const jwtPayload = jwtDecoder(req);
+    try {
+        const result = await report.getReport(req.query.userId || jwtPayload.id);
+        res.send(result);
+    } catch (error) {
+        errorHandler(res, error);
+    }
+});
 
 router.get('/', async (req, res) => {
     const jwtPayload = jwtDecoder(req);
     const localTimeOffset = momentTz(momentTz(), momentTz.tz.guess()).format('Z');
     try {
         const queryCollection = [
-            dashbaord.getAssignedProject(jwtPayload.id),
-            dashbaord.getWorkedOnProject(jwtPayload.id),
-            dashbaord.getProjectInvolvementForWeek(jwtPayload.id, localTimeOffset),
-            dashbaord.getProjectHrByDay(jwtPayload.id, localTimeOffset)
+            dashbaord.getAssignedProject(req.query.userId || jwtPayload.id),
+            dashbaord.getWorkedOnProject(req.query.userId || jwtPayload.id),
+            dashbaord.getProjectInvolvementForWeek(req.query.userId || jwtPayload.id, localTimeOffset),
+            dashbaord.getProjectHrByDay(req.query.userId || jwtPayload.id, localTimeOffset)
         ];
         const [assignedProjects, workedOnProject, projectInvovlement, projectHrByDay] = await Promise.all(queryCollection);
         res.send({assignedProjects, workedOnProject, projectInvovlement, projectHrByDay});
