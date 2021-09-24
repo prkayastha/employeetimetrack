cab82e87950dc3c826a5fdb920791994798b1786
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-09-24 14:05:07 +0930

    generate report

--- nodeServer/controller/report/generateReport.js
+++ nodeServer/controller/report/generateReport.js
@@ -3,11 +3,15 @@ const path = require('path');
 const fs = require('fs');
 
 const moment = require('moment');
+const getReport = require('./getReport');
 const user = require('../../controller/user');
 
+const average = (list) => list.reduce((prev, curr) => prev + curr) / list.length;
+
 module.exports = async function (userId) {
-    
+
     const userInformation = await getUserById(userId);
+    const reportInfo = await getReport(userId);
 
     const doc = new PDFDocument({
         size: 'A4',
@@ -24,6 +28,8 @@ module.exports = async function (userId) {
     generateHeader(doc, headerInformation);
 
     setTableHeader(doc, 180);
+
+    setTableRow(doc, reportInfo)
 
     doc.end();
 
@@ -76,9 +82,44 @@ function setTableHeader(doc, y) {
     doc.text('Productivity', 460, y);
     doc.lineWidth(1);
     doc.lineCap('butt')
-        .moveTo(80, y+25)
-        .lineTo(510, y+25)
+        .moveTo(80, y + 25)
+        .lineTo(510, y + 25)
         .stroke();
+}
+
+function setTableRow(doc, information) {
+    doc.font('Helvetica');
+    const top = 220
+    let totalTime = 0;
+    let totalProductiveScreen = 0;
+    let totalUnproductiveScreen = 0;
+    let productive = [];
+    for (let i = 0; i < information.length; i++) {
+        const row = information[i];
+        totalTime = totalTime + timeToSec(row.timeForProject);
+        totalProductiveScreen = totalProductiveScreen + (row.totalScreen - row.unproductive);
+        totalUnproductiveScreen = totalUnproductiveScreen + row.unproductive;
+        if (row.totalScreen > 0) {
+            productive.push(row.unproductive / row.totalScreen)
+        }
+        doc.text(row.projectName, 80, top + (i * 35), { width: 150 });
+        doc.text(row.timeForProject, 230, top + (i * 35), { width: 100 });
+        doc.text(row.totalScreen - row.unproductive, 310, top + (i * 35));
+        doc.text(row.unproductive, 380, top + (i * 35));
+        doc.text(row.totalScreen == 0 ? 'N/A' : 1 - (row.unproductive / row.totalScreen), 460, top + (i * 35));
+        doc.lineWidth(0.5);
+        doc.lineCap('butt')
+            .moveTo(80, doc.y+20)
+            .lineTo(510, doc.y+20)
+            .stroke();
+    }
+
+    totalTime = secToTime(totalTime);
+    const averageProd = average(productive);
+    doc.text(`Total Time Recorded: ${totalTime}`, 80,doc.y+50, {width: 510, align: 'left'});
+    doc.text(`Total Recorded Productive Screens: ${totalProductiveScreen}`, 80,doc.y, {width: 510, align: 'left'});
+    doc.text(`Total Recorded Unproductive Screens: ${totalUnproductiveScreen}`, 80,doc.y, {width: 510, align: 'left'});
+    doc.text(`Total Productive Rate: ${(1 - averageProd)*100}%`, 80,doc.y, {width: 510, align: 'left'});
 }
 
 function getDates() {
@@ -86,10 +127,28 @@ function getDates() {
     const today = moment();
     const startDate = today.startOf('week').format(formatDate);
     const endDate = today.endOf('week').format(formatDate);
-    return {startDate, endDate}
+    return { startDate, endDate }
 }
 
 async function getUserById(userId) {
     const result = await user.get(userId);
     return result;
+}
+
+function timeToSec(time) {
+    if (time === '00:00:00') return 0;
+    const splited = time.split(':');
+    return parseInt(splited[0])*3600+parseInt(splited[1])*60+parseInt(splited[2]);
+}
+
+function secToTime(sec) {
+    if (sec == 0) return '00:00:00';
+
+    const parted = [];
+    parted.push((sec%60).toLocaleString('en-US', {minimumIntegerDigits: 2}));
+    let minutes = Math.floor(sec/60);
+    parted.push((minutes%60).toLocaleString('en-US', {minimumIntegerDigits: 2}));
+    let hour = Math.floor(minutes/60);
+    parted.push(hour.toLocaleString('en-US', {minimumIntegerDigits: 2}));
+    return parted.reverse().join(':')
 }
