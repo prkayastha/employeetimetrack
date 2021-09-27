0be301f42cd903f68c8818b184e50b5c954ec92a
Prajesh Raj Kayastha <94prajesh@gmail.com>
2021-09-27 20:00:06 +0930

    add projectName

--- nodeServer/controller/dashboard/getProjectHrByDay.js
+++ nodeServer/controller/dashboard/getProjectHrByDay.js
@@ -1,15 +1,17 @@
 const sqlQuery = `SELECT 
 \`task\`.\`projectId\`,
+\`project\`.\`projectName\`,
 SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(\`timer\`.\`endedAt\`, \`timer\`.\`startedAt\`)))) AS \`duration\`,
 DAYOFWEEK(\`timer\`.\`startedAt\`) AS \`dayOfWeek\`
 FROM
 Timers \`timer\`
     INNER JOIN
 Tasks \`task\` ON \`timer\`.\`taskId\` = \`task\`.\`id\`
+INNER JOIN Projects \`project\` on \`project\`.\`id\`=\`task\`.\`projectId\`
 WHERE
 \`timer\`.\`userId\` = :userId
     AND startedAt BETWEEN :startDate AND :endDate
-GROUP BY \`task\`.\`projectId\` , dayOfWeek
+GROUP BY \`task\`.\`projectId\` , \`project\`.\`projectName\` , dayOfWeek
 ORDER BY projectId ASC , dayOfWeek ASC`;
 const models = require('../../models');
 const momentTz = require('moment-timezone');
@@ -33,7 +35,7 @@ module.exports = async function(userId, offset) {
     result.forEach(row => {
         let projectObj = getProjectById(projectList, row.projectId);
         if (!projectObj) {
-            projectList.push({projectId: row.projectId});
+            projectList.push({projectId: row.projectId, projectName: row.projectName});
             projectObj = getProjectById(projectList, row.projectId);
         };
         if (!projectObj['durationByDay']) {
